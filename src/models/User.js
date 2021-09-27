64792341cca1094a72b52e1434ba0816889b1b18
isiagi <isiagigeofrey0@gmail.com>
2021-09-28 00:59:45 +0300

    adds reset password

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
-import JWT from 'jsonwebtoken'
+import JWT from 'jsonwebtoken';
+import crypto from 'crypto';
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -17,31 +18,46 @@ const UserSchema = new mongoose.Schema({
     ],
   },
   password: {
-      type: String,
-      required: [true, 'Please provide password'],
-      minlength: 6
-  }
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
-UserSchema.pre('save', async function (next){
-    if(!this.isModified('password')) {
-        return next();
-    }
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
-    next();
-})
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-UserSchema.methods.matchPassword = async function (password){
-    return await bcrypt.compare(password, this.password)
-}
+UserSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-UserSchema.methods.getSignedJwtToken = function() {
-    return JWT.sign({id: this._id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRATION
-    })
-}
+UserSchema.methods.getSignedJwtToken = function () {
+  return JWT.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRATION,
+  });
+};
+
+UserSchema.methods.getResetToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000) //expire in 10 minutes
+
+  return resetToken;
+};
 
 const User = mongoose.model('User', UserSchema);
 
--- src/routers/index.js
+++ src/routers/index.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.route('/login').post(routers.login);
 router.route('/register').post(routers.register);
+router.route('/forgotPassword').post(routers.forgotPassword)
+router.route('/resetPassword/:resetToken').put(routers.resetPassword)
 
 export default router;
