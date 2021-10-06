879aa52a1a83766fbaa6d7483c696353d6f3da94
isiagi <isiagigeofrey0@gmail.com>
2021-10-06 15:18:56 +0300

    refactor

--- src/config/index.js
+++ src/config/index.js
@@ -1,23 +1,42 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { DEVELOPMENT, PRODUCTION, LOCAL } from './envTypes';
 
-dotenv.config()
+dotenv.config();
 
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
+let DB;
 
-const connectDB = async () => {
-    try {    
-        await mongoose.connect(process.env.DBCONNECT, options)
-        console.log("DB connection established");
-    } catch (error) {
-        console.log("Error connecting", error.message);
-    }
+switch (process.env.NODE_ENV) {
+  case PRODUCTION:
+    DB = process.env.DBCONNECTDEVELOPMENT;
+    break;
+  case DEVELOPMENT:
+    DB = process.env.DBCONNECTDEVELOPMENT;
+    break;
+  case LOCAL:
+    DB = process.env.DBCONNECTLOCAL;
+    break;
 
-    const db = mongoose.connect
-    return db
+  default:
+    DB = process.env.DBCONNECTLOCAL;
+    break;
 }
 
-export default connectDB
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB, options);
+    console.log('DB connection established');
+  } catch (error) {
+    console.log('Error connecting', error.message);
+  }
+
+  const db = mongoose.connect;
+  return db;
+};
+
+export default connectDB;
